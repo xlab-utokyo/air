f6cfea617f62599f824a6fea5e22cfec141adf57
imo <imoxxx@gmail.com>
2020-09-08 13:32:04 +0900

    OG image追加、文言修正

--- js/air.js
+++ js/air.js
@@ -1,5 +1,5 @@
 (($) => {
-	const TIME = 400, DUR = 3000, MW = 1280, MH = 800, SP = 480, THRESHOLD = 0.1;
+	const TIME = 400, DUR = 30000, MW = 1280, MH = 800, SP = 480, THRESHOLD = 0.1;
 	let W, H, VW, sp = false, pc = true, timer;
 	let currentUser = null, thisUser = null, isAlreadyTested = false, peer, room, roomId, processor, isAlreadyPlayed;
 	
@@ -16,7 +16,6 @@
 //			$("#wrap .bg")[0].play();
 		}
 		$("#wrap .bg").show();
-		console.log(1);
 		setSize();
 		$(window).on("orientationchange resize", setSize);
 		$(window).on("scroll", () => {
