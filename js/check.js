c23dcadee1a87c48000b853474c25f677b561346
dadaa <dadaa@users.noreply.github.com>
2020-09-23 09:56:41 +0900

    Update the device list for YOKOHAMA

--- js/check.js
+++ js/check.js
@@ -1,45 +1,24 @@
 const DEVICES = {
-  "linz-1": {
-    label: "LINZ 1",
-    room: "linz-1-production",
+  "yokohama-1": {
+    label: "YOKOHAMA 1",
+    room: "yokohama-1",
     network: "sfu",
     key: "42d2a8f3-ed64-48af-8928-902bd4e4f5bc",
     cid: "52c30248-e069-11ea-87d0-0242ac130003",
   },
-  "linz-2": {
-    label: "LINZ 2",
-    room: "linz-2-production",
+  "yokohama-2": {
+    label: "YOKOHAMA 2",
+    room: "yokohama-2",
     network: "sfu",
     key: "4bf93d11-4e49-40a4-8a25-f33c033cf1fe",
     cid: "52c3048c-e069-11ea-87d0-0242ac130003",
   },
-  "linz-3": {
-    label: "LINZ 3",
-    room: "linz-3-production",
+  "yokohama-3": {
+    label: "YOKOHAMA 3",
+    room: "yokohama-3",
     network: "sfu",
     key: "4409fcf1-896a-4d12-8e1b-bb10175e24e3",
     cid: "52c305e0-e069-11ea-87d0-0242ac130003",
-  },
-  "tokyo-1": {
-    label: "TOKYO 1",
-    room: "tokyo-1-production",
-    network: "sfu",
-    key: "f061f8cb-d48f-49b4-8fa7-691e7ec19b48",
-    cid: "9f7ea436-e6a7-11ea-adc1-0242ac120002",
-  },
-  "tokyo-2": {
-    label: "TOKYO 2",
-    room: "tokyo-2-production-2",
-    network: "sfu",
-    key: "0c58b2e1-96e1-47c5-82f7-cc6a7419600a",
-    cid: "9f7ea68e-e6a7-11ea-adc1-0242ac120002",
-  },
-  "tokyo-3": {
-    label: "TOKYO 3",
-    room: "tokyo-3-production-2",
-    network: "sfu",
-    key: "6e465825-3b1c-4070-a0be-9b3987f8481c",
-    cid: "9f7ea8aa-e6a7-11ea-adc1-0242ac120002",
   },
 };
 
