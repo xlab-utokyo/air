8536f7cd6f3d0d7a16aebce471ed87bfe50d6e08
imo <34334834+imoxxx@users.noreply.github.com>
2021-09-29 20:40:05 +0900

    Update check.js

--- js/check.js
+++ js/check.js
@@ -1,21 +1,21 @@
 const DEVICES = {
   "yokohama-1": {
-    label: "YOKOHAMA 1",
-    room: "yokohama-1",
+    label: "LINZ 1",
+    room: "linz-1",
     network: "sfu",
     key: "42d2a8f3-ed64-48af-8928-902bd4e4f5bc",
     cid: "52c30248-e069-11ea-87d0-0242ac130003",
   },
   "yokohama-2": {
-    label: "YOKOHAMA 2",
-    room: "yokohama-2",
+    label: "LINZ 2",
+    room: "linz-2",
     network: "sfu",
     key: "4bf93d11-4e49-40a4-8a25-f33c033cf1fe",
     cid: "52c3048c-e069-11ea-87d0-0242ac130003",
   },
   "yokohama-3": {
-    label: "YOKOHAMA 3",
-    room: "yokohama-3",
+    label: "LINZ 3",
+    room: "linz-3",
     network: "sfu",
     key: "4409fcf1-896a-4d12-8e1b-bb10175e24e3",
     cid: "52c305e0-e069-11ea-87d0-0242ac130003",
