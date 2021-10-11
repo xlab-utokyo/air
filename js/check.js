15b215672210cb516abe33153bb443b561000a8e
imo <imoxxx@gmail.com>
2021-10-11 21:44:48 +0900

    マシン名をTOKYOに変更

--- js/check.js
+++ js/check.js
@@ -1,26 +1,19 @@
 const DEVICES = {
-  "linz-1": {
-    label: "LINZ 1",
-    room: "linz-1",
+  "tokyo-1": {
+    label: "TOKYO 1",
+    room: "tokyo-1",
     network: "sfu",
     key: "42d2a8f3-ed64-48af-8928-902bd4e4f5bc",
     cid: "52c30248-e069-11ea-87d0-0242ac130003",
   },
-  "linz-2": {
-    label: "LINZ 2",
-    room: "linz-2",
+  "tokyo-2": {
+    label: "TOKYO 2",
+    room: "tokyo-2",
     network: "sfu",
     key: "4bf93d11-4e49-40a4-8a25-f33c033cf1fe",
     cid: "52c3048c-e069-11ea-87d0-0242ac130003",
-  },
-  "linz-3": {
-    label: "LINZ 3",
-    room: "linz-3",
-    network: "sfu",
-    key: "4409fcf1-896a-4d12-8e1b-bb10175e24e3",
-    cid: "52c305e0-e069-11ea-87d0-0242ac130003",
-  },
-};
+  }
+ };
 
 document.addEventListener("DOMContentLoaded", () => {
   const url = new URL(document.URL);
